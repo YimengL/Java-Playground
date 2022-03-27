6332d289272c6125e136cf46ed05b7d253baeed3
Yimeng Li <yimengcsu@gmail.com>
2022-03-27 17:56:01 +0200

    sq-ch5-ex7 initial commit

---
+++ spring_start_here/sq-ch5-ex7/src/main/java/processors/CommentProcessor.java
@@ -0,0 +1,16 @@
+package processors;
+
+import model.Comment;
+
+public class CommentProcessor {
+
+    private Comment comment;
+
+    public Comment getComment() {
+        return comment;
+    }
+
+    public void setComment(Comment comment) {
+        this.comment = comment;
+    }
+}
