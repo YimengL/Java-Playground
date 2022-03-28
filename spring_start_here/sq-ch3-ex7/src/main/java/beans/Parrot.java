eb1e910393d42e7080fef7c4f913a0e35d5e8694
YimengL <yimengcsu@gmail.com>
2022-03-28 15:34:47 +0200

    sq-ch3-ex7

---
+++ spring_start_here/sq-ch3-ex7/src/main/java/beans/Parrot.java
@@ -0,0 +1,32 @@
+package beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class Parrot {
+
+    private String name = "Koko";
+
+    private final Person person;
+
+
+    @Autowired
+    public Parrot(Person person) {
+        this.person = person;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Parrot : " + name;
+    }
+}
