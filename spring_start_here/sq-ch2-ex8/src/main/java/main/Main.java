b24c527853b6d8319c554598f379c7762024cb03
YimengL <yimengcsu@gmail.com>
2022-03-28 15:32:32 +0200

    sq-ch2-ex8

---
+++ spring_start_here/sq-ch2-ex8/src/main/java/main/Main.java
@@ -0,0 +1,25 @@
+package main;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import config.ProjectConfig;
+
+import java.util.function.Supplier;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Parrot x = new Parrot();
+        x.setName("Kiki");
+
+        Supplier<Parrot> parrotSupplier = () -> x;
+
+        context.registerBean("parrot1", Parrot.class, parrotSupplier);
+
+        Parrot p = context.getBean(Parrot.class);
+        System.out.println(p.getName());
+    }
+}
