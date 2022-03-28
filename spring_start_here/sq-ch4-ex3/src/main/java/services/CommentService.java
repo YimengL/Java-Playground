1404f8e4c73e806a20b4f447bd1c164147d0b365
YimengL <yimengcsu@gmail.com>
2022-03-28 15:31:18 +0200

    sq-ch4-ex3

---
+++ spring_start_here/sq-ch4-ex3/src/main/java/services/CommentService.java
@@ -0,0 +1,23 @@
+package services;
+
+import model.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import proxies.CommentNotificationProxy;
+import repositories.CommentRepository;
+
+// Spring creates a bean of this class and adds it to its context
+@Component
+public class CommentService {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private CommentNotificationProxy commentNotificationProxy;
+
+    public void publishComment(Comment comment) {
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+    }
+}
