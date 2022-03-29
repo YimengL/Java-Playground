2fe81fd8106c75ee4837dba43f9951b71a2d6b1b
Yimeng Li <yimengcsu@gmail.com>
2022-03-29 20:21:40 +0200

    sq-ch6-ex1 initial commit

---
+++ spring_start_here/sq-ch6-ex1/src/main/java/aspects/LoggingAspect.java
@@ -0,0 +1,11 @@
+package aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class LoggingAspect {
+
+    public void log() {
+
+    }
+}
---
+++ spring_start_here/sq-ch6-ex1/src/main/java/config/ProjectConfig.java
@@ -0,0 +1,23 @@
+package config;
+
+import aspects.LoggingAspect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+// We use @ComponentScan to tell Spring where to search for classes annotated with stereotype annotation.
+@ComponentScan(basePackages = "services")
+@EnableAspectJAutoProxy // Enables the aspects mechanism in our Spring app
+public class ProjectConfig {
+
+    /**
+     * Adds an instance of the {@link LoggingAspect} class to the Spring context.
+     */
+    @Bean
+    public LoggingAspect aspect() {
+        return new LoggingAspect();
+    }
+    
+}
