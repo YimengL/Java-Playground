c31c101eb0f60c286995ab09596cb9b182e0b236
Yimeng Li <yimengcsu@gmail.com>
2022-03-31 14:05:56 +0200

    Finished sq-ch6-ex1

--- spring_start_here/sq-ch6-ex1/src/main/java/aspects/LoggingAspect.java
+++ spring_start_here/sq-ch6-ex1/src/main/java/aspects/LoggingAspect.java
@@ -1,11 +1,22 @@
 package aspects;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+
+import java.util.logging.Logger;
 
 @Aspect
 public class LoggingAspect {
 
-    public void log() {
+    private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
 
+    // Defines which are intercepted method
+    @Around("execution(* services.*.*(..))")
+    public void log(ProceedingJoinPoint joinPoint) throws Throwable {
+        logger.info("Method will execute");
+        // Delegates to the actual intercepted method
+        joinPoint.proceed();
+        logger.info("Method executed");
     }
 }
--- spring_start_here/sq-ch6-ex1/src/main/java/config/ProjectConfig.java
+++ spring_start_here/sq-ch6-ex1/src/main/java/config/ProjectConfig.java
@@ -19,5 +19,4 @@ public class ProjectConfig {
     public LoggingAspect aspect() {
         return new LoggingAspect();
     }
-    
 }
