e7f3f1e89da503bc59f5385c00096d55b331d992
Yimeng Li <yimengcsu@gmail.com>
2022-04-07 23:16:53 +0200

    sq-ch8-ex1

---
+++ spring_start_here/sq-ch8-ex1/src/main/java/com/example/SqCh8Ex1Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh8Ex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh8Ex1Application.class, args);
+	}
+
+}
