8051bd218304ce3270aeb42737e0bd71089e49da
Yimeng Li <yimengcsu@gmail.com>
2022-04-17 18:59:32 +0200

    sq-ch10-ex4 Init

---
+++ spring_start_here/sq-ch10-ex4/src/main/java/com/example/SqCh10Ex4Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh10Ex4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh10Ex4Application.class, args);
+	}
+
+}
