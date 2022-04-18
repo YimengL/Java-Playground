7454772b0e7d5a86621bcf35c4ea04e1e1a103a9
Yimeng Li <yimengcsu@gmail.com>
2022-04-18 15:22:46 +0200

    sq-ch10-ex4 DONE

--- spring_start_here/sq-ch10-ex4/src/main/java/com/example/Main.java
+++ spring_start_here/sq-ch10-ex4/src/main/java/com/example/Main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SqCh10Ex4Application {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SqCh10Ex4Application.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
--- spring_start_here/sq-ch10-ex4/src/test/java/com/example/MainTests.java
+++ spring_start_here/sq-ch10-ex4/src/test/java/com/example/MainTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SqCh10Ex4ApplicationTests {
+class MainTests {
 
 	@Test
 	void contextLoads() {
