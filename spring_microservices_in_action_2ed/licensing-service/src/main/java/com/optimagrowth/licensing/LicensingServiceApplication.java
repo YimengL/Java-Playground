e8ac2b06cf2e57e53c1c5e23780620655fcf55fc
Yimeng Li <yimengcsu@gmail.com>
2022-05-26 19:50:51 +0200

    licensing service

---
+++ spring_microservices_in_action_2ed/licensing-service/src/main/java/com/optimagrowth/licensing/LicensingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.optimagrowth.licensing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicensingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicensingServiceApplication.class, args);
+	}
+
+}
---
+++ spring_microservices_in_action_2ed/licensing-service/src/test/java/com/optimagrowth/licensing/LicensingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optimagrowth.licensing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicensingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
