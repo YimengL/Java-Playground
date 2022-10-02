4eb08adb2989c275ac130d9f1f51d5554641ff22
Yimeng Li <yimengcsu@gmail.com>
2022-10-02 15:11:10 +0200

    Update bootstrap file

--- spring_microservices_in_action_2ed/chapter5/configserver/src/main/java/com/optimagrowth/configserver/ConfigurationServerApplication.java
+++ spring_microservices_in_action_2ed/chapter5/configserver/src/main/java/com/optimagrowth/configserver/ConfigurationServerApplication.java
@@ -2,10 +2,17 @@ package com.optimagrowth.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+// Our Config service is a Spring Boot application, so we must mark it with the @SpringBootApplication annotation
 @SpringBootApplication
+// Enables the service as a Spring Cloud Config service
+@EnableConfigServer
 public class ConfigurationServerApplication {
 
+	/**
+	 * The main method launches the service and starts the Spring container.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigurationServerApplication.class, args);
 	}
