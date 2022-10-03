369ebd8a414cf09d9d18d4da850f446176343601
Yimeng Li <yimengcsu@gmail.com>
2022-10-03 13:05:14 +0200

    Central place to retrieve custom config

---
+++ spring_microservices_in_action_2ed/chapter5/licensing-service/src/main/java/com/optimagrowth/licensing/config/ServiceConfig.java
@@ -0,0 +1,13 @@
+package com.optimagrowth.licensing.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "example")
+@Getter @Setter
+public class ServiceConfig {
+    private String property;
+}
