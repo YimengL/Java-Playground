7c64e8c6b932884cf9742bd8f70f34cb3224a458
Yimeng Li <yimengcsu@gmail.com>
2022-10-09 13:57:27 +0200

    Create OrganizationService.java

---
+++ spring_microservices_in_action_2ed/chapter6/organization-service/src/main/java/com/optimagrowth/organizationservice/service/OrganizationService.java
@@ -0,0 +1,36 @@
+package com.optimagrowth.organizationservice.service;
+
+import com.optimagrowth.organizationservice.model.Organization;
+import com.optimagrowth.organizationservice.repository.OrganizationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+
+@Service
+public class OrganizationService {
+
+    @Autowired
+    private OrganizationRepository repository;
+
+    public Organization findById(String organizationId) {
+        Optional<Organization> opt = repository.findById(organizationId);
+        return opt.orElse(null);
+    }
+
+    public Organization create(Organization organization) {
+        organization.setId(UUID.randomUUID().toString());
+        organization = repository.save(organization);
+        return organization;
+    }
+
+    public void update(Organization organization) {
+        repository.save(organization);
+    }
+
+    public void delete(Organization organization) {
+        repository.deleteById(organization.getId());
+    }
+}
