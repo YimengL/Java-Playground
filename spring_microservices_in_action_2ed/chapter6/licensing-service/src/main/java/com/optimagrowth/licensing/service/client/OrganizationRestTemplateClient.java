740135e44db50fa57d8083bf69006acf6fde0655
Yimeng Li <yimengcsu@gmail.com>
2022-10-09 19:13:19 +0200

    Create OrganizationRestTemplateClient.java

---
+++ spring_microservices_in_action_2ed/chapter6/licensing-service/src/main/java/com/optimagrowth/licensing/service/client/OrganizationRestTemplateClient.java
@@ -0,0 +1,25 @@
+package com.optimagrowth.licensing.service.client;
+
+import com.optimagrowth.licensing.model.Organization;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class OrganizationRestTemplateClient {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public Organization getOrganization(String organizationId) {
+        ResponseEntity<Organization> restExchange =
+                restTemplate.exchange(
+                        "http://organization-service/v1/organization/{organizationId}",
+                        HttpMethod.GET,
+                        null, Organization.class, organizationId);
+
+        return restExchange.getBody();
+    }
+}
