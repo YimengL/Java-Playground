8dc44b23908ea9fd04f666b26c7707e56234e204
Yimeng Li <yimengcsu@gmail.com>
2022-10-16 14:59:27 +0200

    Add Eureka Server in Docker-Compose

--- spring_microservices_in_action_2ed/chapter6/eurekaserver/src/main/java/com/optimagrowth/eurekaserver/EurekaServerApplication.java
+++ spring_microservices_in_action_2ed/chapter6/eurekaserver/src/main/java/com/optimagrowth/eurekaserver/EurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.optimagrowth.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer // Enables the Eureka Server in the Spring service
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
