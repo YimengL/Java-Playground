162ff36dc1dfd6868ab67b9f0bc9969362249c92
Yimeng Li <yimengcsu@gmail.com>
2022-10-23 19:16:27 +0200

    Add API Gateway Server to docker-compose

--- spring_microservices_in_action_2ed/chapter8/gatewayserver/src/main/java/com/optimagrowth/gatewayserver/ApiGatewayServerApplication.java
+++ spring_microservices_in_action_2ed/chapter8/gatewayserver/src/main/java/com/optimagrowth/gatewayserver/ApiGatewayServerApplication.java
@@ -2,8 +2,10 @@ package com.optimagrowth.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ApiGatewayServerApplication {
 
 	public static void main(String[] args) {
