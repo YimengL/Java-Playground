548724cd80799e75f2d7e995b0a21e06129ee4a7
Yimeng Li <yimengcsu@gmail.com>
2022-10-23 17:19:40 +0200

    Gateway Server Skeleton Code

---
+++ spring_microservices_in_action_2ed/chapter8/gatewayserver/src/main/java/com/optimagrowth/gatewayserver/ApiGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.optimagrowth.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServerApplication.class, args);
+	}
+
+}
