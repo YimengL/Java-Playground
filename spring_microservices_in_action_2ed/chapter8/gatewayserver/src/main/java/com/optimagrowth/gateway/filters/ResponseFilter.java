a59babec2b920c29b97af62e01bbe2771e6e3994
Yimeng Li <yimengcsu@gmail.com>
2022-10-28 17:45:58 +0200

    Finished the Response Filter for Gateway

--- spring_microservices_in_action_2ed/chapter8/gatewayserver/src/main/java/com/optimagrowth/gateway/filters/ResponseFilter.java
+++ spring_microservices_in_action_2ed/chapter8/gatewayserver/src/main/java/com/optimagrowth/gateway/filters/ResponseFilter.java
@@ -4,10 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import reactor.core.publisher.Mono;
-
 
 
 @Configuration
@@ -18,12 +18,20 @@ public class ResponseFilter {
     @Autowired
     FilterUtils filterUtils;
 
+    @Bean
     public GlobalFilter postGlobalFilter() {
         return (exchange, chain) -> {
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
                 HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+                // Grabs the correlation ID that was passed in to the original HTTP request
                 String correlationId = filterUtils.getCorrelationId(requestHeaders);
                 logger.debug("Adding the correlation id to the outbound headers. {}", correlationId);
+
+                exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, correlationId);
+
+                // Logs the outgoing request URI so that you have "bookends" that show the incoming and outgoing entry
+                // of the user's request into the gateway
+                logger.debug("Completing outing request for {}.", exchange.getRequest().getURI());
             }));
         };
     }
