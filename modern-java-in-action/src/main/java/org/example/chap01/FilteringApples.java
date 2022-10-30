a9b2f0a6c22d8cc9ac04f94318e77c57e7a6348f
Yimeng Li <yimengcsu@gmail.com>
2022-10-30 13:50:19 +0100

    Skeleton Code

---
+++ modern-java-in-action/src/main/java/org/example/chap01/FilteringApples.java
@@ -0,0 +1,61 @@
+package org.example.chap01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FilteringApples {
+
+    public static void main(String[] args) {
+
+        List<Apple> inventory = Arrays.asList(
+                new Apple(80, "green"),
+                new Apple(155, "green"),
+                new Apple(126, "red"));
+
+
+
+    }
+
+    public static List<Apple> filterGreenApples(List<Apple> inventory) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if ("green".equals(apple.getColor())) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    public static class Apple {
+        private int weight = 0;
+        private String color = "";
+
+        public Apple(int weight, String color) {
+            this.weight = weight;
+            this.color = color;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+
+        @SuppressWarnings("boxing")
+        @Override
+        public String toString() {
+            return String.format("Apple{color='%s', weight=%d}", color, weight);
+        }
+    }
+}
