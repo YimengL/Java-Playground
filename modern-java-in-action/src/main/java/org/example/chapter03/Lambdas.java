4671b87e1e69307503705c29137d73e67d374f40
YimengL <yimengcsu@gmail.com>
2022-11-18 22:24:55 +0100

    Create Lambdas.java

---
+++ modern-java-in-action/src/main/java/org/example/chapter03/Lambdas.java
@@ -0,0 +1,11 @@
+package org.example.chapter03;
+
+public class Lambdas {
+    public static void main(String[] args) {
+        // Simple example
+        Runnable r = () -> System.out.println("Hello");
+        r.run();
+
+
+    }
+}
