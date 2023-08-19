33503b2a688249c20926b50b4e49c49f535dbbd5
YimengL <yimengcsu@gmail.com>
2023-08-19 18:50:40 +0200

    SMIA - ch04 - Lazy Evaluation

--- modern-java-in-action/src/main/java/org/example/chap04/HighCaloriesNames.java
+++ modern-java-in-action/src/main/java/org/example/chap04/HighCaloriesNames.java
@@ -7,8 +7,14 @@ public class HighCaloriesNames {
 
     public static void main(String[] args) {
         List<String> names = Dish.menu.stream()
-                .filter(dish -> dish.getCalories() > 300)
-                .map(Dish::getName)
+                .filter(dish -> {
+                    System.out.println("filtering:" + dish.getName());
+                    return dish.getCalories() > 300;
+                })
+                .map(dish -> {
+                    System.out.println("mapping:" + dish.getName());
+                    return dish.getName();
+                })
                 .limit(3)
                 .collect(Collectors.toList());
         System.out.println(names);
