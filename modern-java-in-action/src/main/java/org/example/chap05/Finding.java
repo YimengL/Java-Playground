f512369ad4678395cbacffb1d5a26b36fef74ff8
yimengli <yimengcsu@gmail.com>
2023-08-20 19:42:50 +0200

    MJIA - ch05 - Finding

---
+++ modern-java-in-action/src/main/java/org/example/chap05/Finding.java
@@ -0,0 +1,36 @@
+package org.example.chap05;
+
+import org.example.chap04.Dish;
+
+import java.util.Optional;
+
+public class Finding {
+
+    public static void main(String[] args) {
+        if (isVegetarianFriendlyMenu()) {
+            System.out.println("Vegetarian friendly");
+        }
+
+        System.out.println(isHealthyMenu());
+        System.out.println(isHealthyMenu2());
+
+        Optional<Dish> dish = findVegetarianDish();
+        dish.ifPresent(d -> System.out.println(d.getName()));
+    }
+
+    private static boolean isVegetarianFriendlyMenu() {
+        return Dish.menu.stream().anyMatch(Dish::isVegetarian);
+    }
+
+    private static boolean isHealthyMenu() {
+        return Dish.menu.stream().allMatch(d -> d.getCalories() < 1000);
+    }
+
+    private static boolean isHealthyMenu2() {
+        return Dish.menu.stream().noneMatch(d -> d.getCalories() >= 1000);
+    }
+
+    private static Optional<Dish> findVegetarianDish() {
+        return Dish.menu.stream().filter(Dish::isVegetarian).findAny();
+    }
+}
