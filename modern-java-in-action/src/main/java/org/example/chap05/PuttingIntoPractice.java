1020651c87b322a78e319d374764bce2e2343b3c
yimengli <yimengcsu@gmail.com>
2023-08-24 23:51:20 +0200

    MJIA - ch05 - Transaction & PuttingIntoPractice skeleton code

---
+++ modern-java-in-action/src/main/java/org/example/chap05/PuttingIntoPractice.java
@@ -0,0 +1,52 @@
+package org.example.chap05;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PuttingIntoPractice {
+
+    public static void main(String[] args) {
+        Trader raoul = new Trader("Raoul", "Cambridge");
+        Trader mario = new Trader("Mario", "Milan");
+        Trader alan = new Trader("Alan", "Cambridge");
+        Trader brian = new Trader("Brian", "Cambridge");
+
+        List<Transaction> transactions = Arrays.asList(
+                new Transaction(brian, 2011, 300),
+                new Transaction(raoul, 2012, 1000),
+                new Transaction(raoul, 2011, 400),
+                new Transaction(mario, 2012, 710),
+                new Transaction(mario, 2012, 700),
+                new Transaction(alan, 2012, 950)
+        );
+
+        // Query 1: Find all transactions from year 2011 and sort them by value (small to high)
+        List<Transaction> tr2011 = transactions.stream()
+                .filter(transaction -> transaction.getYear() == 2011)
+                .sorted(Comparator.comparing(Transaction::getValue))
+                .collect(Collectors.toList());
+        System.out.println(tr2011);
+
+        // Query 2: What are all the unique cities where the traders work
+        List<String> cities = transactions.stream()
+                .map(Transaction::getTrader)
+                .map(Trader::getCity)
+                .distinct()
+                .collect(Collectors.toList());
+        System.out.println(cities);
+
+        // Query 3: Find all traders from Cambridge and sort them by name
+
+        // Query 4: Return a string of all traders' names sorted alphabetically
+
+        // Query 5: Are any traders based in Milan?
+
+        // Query 6: Print the values of all transactions from the traders living in Cambridge.
+
+        // Query 7: What's the highest value of all the transactions?
+
+        // Query 8: Find the transaction with the smallest value.
+    }
+}
