ecf57922a1f521f15a3c93aea49659b7595912e5
YimengL <yimengcsu@gmail.com>
2023-08-26 14:08:52 +0200

    SMIA - ch05 - PuttingIntoPractice

--- modern-java-in-action/src/main/java/org/example/chap05/PuttingIntoPractice.java
+++ modern-java-in-action/src/main/java/org/example/chap05/PuttingIntoPractice.java
@@ -3,6 +3,7 @@ package org.example.chap05;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class PuttingIntoPractice {
@@ -38,15 +39,43 @@ public class PuttingIntoPractice {
         System.out.println(cities);
 
         // Query 3: Find all traders from Cambridge and sort them by name
+        List<Trader> traders = transactions.stream()
+                .map(Transaction::getTrader)
+                .filter(t -> "Cambridge".equals(t.getCity()))
+                .distinct()
+                .sorted(Comparator.comparing(Trader::getName))
+                .collect(Collectors.toList());
+        System.out.println(traders);
 
         // Query 4: Return a string of all traders' names sorted alphabetically
+        String traderStr = transactions.stream()
+                .map(Transaction::getTrader)
+                .map(Trader::getName)
+                .distinct()
+                .reduce("", (name1, name2) -> name1 + name2);
+        System.out.println(traderStr);
 
         // Query 5: Are any traders based in Milan?
+        boolean milanBased = transactions.stream()
+                .anyMatch(transaction -> transaction.getTrader().getCity().equals("Milan"));
+        System.out.println(milanBased);
 
         // Query 6: Print the values of all transactions from the traders living in Cambridge.
+        transactions.stream().filter(transaction -> "Cambridge".equals(transaction.getTrader().getCity()))
+                .map(Transaction::getValue)
+                .forEach(System.out::println);
 
         // Query 7: What's the highest value of all the transactions?
+        int highestValue = transactions.stream()
+                .map(Transaction::getValue)
+                .reduce(0, Integer::max);
+        System.out.println(highestValue);
 
         // Query 8: Find the transaction with the smallest value.
+        Optional<Transaction> smallestTransaction = transactions.stream()
+                .min(Comparator.comparing(Transaction::getValue));
+        // Here I cheat a bit by converting the found transaction (if any) to a String
+        // so that I can use a default String if no transactions are found (i.e. the Stream is empty).
+        System.out.println(smallestTransaction.map(String::valueOf).orElse("No transaction found"));
     }
 }
