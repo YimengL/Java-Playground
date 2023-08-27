2663b6f8c7fb4f2b6e4fba18043810bb586feef1
YimengL <yimengcsu@gmail.com>
2023-08-27 14:51:10 +0200

    MJIA - ch05 - NumericStreams final

--- modern-java-in-action/src/main/java/org/example/chap05/NumericStreams.java
+++ modern-java-in-action/src/main/java/org/example/chap05/NumericStreams.java
@@ -5,6 +5,9 @@ import org.example.chap04.Dish;
 import java.util.Arrays;
 import java.util.List;
 import java.util.OptionalInt;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 
 public class NumericStreams {
     public static void main(String[] args) {
@@ -26,6 +29,25 @@ public class NumericStreams {
         System.out.println(max);
 
         // numeric ranges
+        IntStream evenNumbers = IntStream.rangeClosed(1, 100)
+                .filter(n -> n % 2 == 0);
+        System.out.println(evenNumbers.count());
 
+        Stream<int[]> pythagoreanTriples = IntStream.rangeClosed(1, 100).boxed()
+                .flatMap(a -> IntStream.rangeClosed(a, 100)
+                        .filter(b -> Math.sqrt(a * a + b * b) % 1 == 0).boxed()
+                        .map(b -> new int[] {a, b, (int) Math.sqrt(a * a + b * b)}));
+        pythagoreanTriples.forEach(t -> System.out.println(t[0] + ", " + t[1] + ", " + t[2]));
+
+        Stream<int[]> pythagoreanTriples2 = IntStream.rangeClosed(1, 100).boxed()
+                .flatMap(a -> IntStream.rangeClosed(a, 100)
+                        .mapToObj(b -> new double[] {a, b, Math.sqrt(a * a + b * b)})
+                        .filter(t -> t[2] % 1 == 0))
+                .map(array -> Arrays.stream(array).mapToInt(a -> (int) a).toArray());
+        pythagoreanTriples2.forEach(t -> System.out.println(t[0] + ", " + t[1] + ", " + t[2]));
+    }
+
+    public static boolean isPerfectSquare(int n) {
+        return Math.sqrt(n) % 1 == 0;
     }
 }
