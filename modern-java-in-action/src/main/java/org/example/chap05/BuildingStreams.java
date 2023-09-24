35afa88b81b6e4760072a22e1a4813e1b9060f3b
yimengli <yimengcsu@gmail.com>
2023-09-24 16:24:54 +0200

    MJIA - ch05 - Stream Iterate

--- modern-java-in-action/src/main/java/org/example/chap05/BuildingStreams.java
+++ modern-java-in-action/src/main/java/org/example/chap05/BuildingStreams.java
@@ -5,6 +5,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class BuildingStreams {
@@ -19,6 +20,31 @@ public class BuildingStreams {
         int[] numbers = {2, 3, 5, 7, 11, 13};
         System.out.println(Arrays.stream(numbers).sum());
 
+        // Stream.iterate
+        Stream.iterate(0, n -> n + 2)
+                .limit(10)
+                .forEach(System.out::println);
+
+        // Fibonacci with iterate
+        Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1], t[0] + t[1]})
+                .limit(10)
+                .forEach(t -> System.out.printf("(%d, %d)", t[0], t[1]));
+
+        Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1], t[0] + t[1]})
+                .limit(10)
+                .map(t -> t[0])
+                .forEach(System.out::println);
+
+        // random stream of doubles with Stream.generate
+        Stream.generate(Math::random)
+                .limit(10)
+                .forEach(System.out::println);
+
+        // Stream of 1s with Stream.generate
+        IntStream.generate(() -> 1)
+                .limit(5)
+                .forEach(System.out::println);
+
         long uniqueWords = 0;
         try (Stream<String> lines = Files.lines(Paths.get("src/main/resources/chap05/data.txt"), Charset.defaultCharset())) {
             uniqueWords = lines.flatMap(line -> Arrays.stream(line.split(" ")))
@@ -27,6 +53,6 @@ public class BuildingStreams {
         } catch (IOException e) {
             System.out.println("Failed to open the file");
         }
-        System.out.println(uniqueWords);
+        System.out.println("There are " + uniqueWords + " unique words in data.txt");
     }
 }
