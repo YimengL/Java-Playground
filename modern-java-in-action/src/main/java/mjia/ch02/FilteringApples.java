3523cc671a4cfc7ff25f4c8b194a8adb41355987
Yimeng Li <yimengcsu@gmail.com>
2024-09-22 12:59:54 +0200

    MJIA - ch02 - FilteringApple skeleton

---
+++ modern-java-in-action/src/main/java/mjia/ch02/FilteringApples.java
@@ -0,0 +1,12 @@
+package mjia.ch02;
+
+public class FilteringApples {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    enum Color {
+        RED,
+        GREEN
+    }
+}
