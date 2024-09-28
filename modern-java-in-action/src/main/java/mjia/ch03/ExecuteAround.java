3e551b3adbbf3bb8c4d4b60ea8c412fde6bf43f3
Yimeng Li <yimengcsu@gmail.com>
2024-09-28 12:32:47 +0200

    MJIA - ch03 - ExecuteAround FINISHED

--- modern-java-in-action/src/main/java/mjia/ch03/ExecuteAround.java
+++ modern-java-in-action/src/main/java/mjia/ch03/ExecuteAround.java
@@ -1,15 +1,42 @@
 package mjia.ch03;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class ExecuteAround {
 
-    public static void main(String[] args) {
+    private static final String FILE = ExecuteAround.class.getClassLoader().getResource("chap03/data.txt").getPath();
 
+    public static void main(String[] args) throws IOException {
+        // Method we want to refactor to make more flexible
+        String result = processFileLimited();
+        System.out.println(result);
+
+        System.out.println("---");
+
+        String oneLine = processFile((BufferedReader b) -> b.readLine());
+        System.out.println(oneLine);
+
+        String twoLines = processFile((BufferedReader b) -> b.readLine() + b.readLine());
+        System.out.println(twoLines);
+    }
+
+    public static String processFileLimited() throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(FILE))) {
+            return br.readLine();
+        }
+    }
+
+    public static String processFile(BufferedReaderProcessor p) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(FILE))) {
+            return p.process(br);
+        }
     }
 
     @FunctionalInterface
     public interface BufferedReaderProcessor {
-        String process(BufferedReader b) throws Exception;
+        String process(BufferedReader b) throws IOException;
     }
 }
