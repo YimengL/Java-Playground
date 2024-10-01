d08a0d5532af9f27c6cee33338efe98e1f1cf905
Yimeng Li <yimengcsu@gmail.com>
2024-10-01 11:24:00 +0200

    MJIA - ch05 - Mapping  FINISHED

--- modern-java-in-action/src/main/java/mjia/ch05/Mapping.java
+++ modern-java-in-action/src/main/java/mjia/ch05/Mapping.java
@@ -26,5 +26,14 @@ public class Mapping {
                 .flatMap(Arrays::stream)
                 .distinct()
                 .forEach(System.out::println);
+
+        // flatMap
+        List<Integer> numbers1 = Arrays.asList(1, 2, 3);
+        List<Integer> numbers2 = Arrays.asList(3, 4);
+        List<int[]> pairs = numbers1.stream()
+                .flatMap(i -> numbers2.stream().map(j -> new int[] {i, j}))
+                .filter(pair -> (pair[0] + pair[1]) % 3 == 0)
+                .collect(Collectors.toList());
+        pairs.forEach(pair -> System.out.printf("(%d, %d)", pair[0], pair[1]));
     }
 }
