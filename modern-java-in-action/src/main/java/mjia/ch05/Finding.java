54c4d9a3d7556bc1ec89e457b42f602dd9eac77e
Yimeng Li <yimengcsu@gmail.com>
2024-10-01 11:43:31 +0200

    MJIA - ch05 - Finding  FINISHED

---
+++ modern-java-in-action/src/main/java/mjia/ch05/Finding.java
@@ -0,0 +1,33 @@
+package mjia.ch05;
+
+import java.util.Optional;
+
+public class Finding {
+    public static void main(String[] args) {
+        if (isVegetarianFriendlyMenu()) {
+            System.out.println("Vegetarian friendly");
+        }
+
+        System.out.println(isHealthyMenu());
+        System.out.println(isHealthyMenu2());
+
+        Optional<Dish> dish = findVegetarianDish();
+        dish.ifPresent(d -> System.out.println(d.getName()));
+    }
+
+    private static boolean isVegetarianFriendlyMenu() {
+        return Dish.menu.stream().anyMatch(Dish::isVegetarian);
+    }
+
+    private static boolean isHealthyMenu() {
+        return Dish.menu.stream().allMatch(dish -> dish.getCalories() < 1000);
+    }
+
+    private static boolean isHealthyMenu2() {
+        return Dish.menu.stream().noneMatch(dish -> dish.getCalories() >= 1000);
+    }
+
+    private static Optional<Dish> findVegetarianDish() {
+        return Dish.menu.stream().filter(Dish::isVegetarian).findAny();
+    }
+}
