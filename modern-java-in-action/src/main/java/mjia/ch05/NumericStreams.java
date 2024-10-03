ebb1f98e15e6be0fb3a661abd4a8bb832f53407e
yimengli <yimengcsu@gmail.com>
2024-10-03 15:32:40 +0200

    MJIA - ch05 - NumericStreams FINISHED

---
+++ modern-java-in-action/src/main/java/mjia/ch05/NumericStreams.java
@@ -0,0 +1,47 @@
+package mjia.ch05;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class NumericStreams {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(3, 4, 5, 1, 2);
+
+        Arrays.stream(numbers.toArray())
+                .forEach(System.out::println);
+        int calories = Dish.menu.stream()
+                .mapToInt(Dish::getCalories)
+                .sum();
+        System.out.println("Number of calories: " + calories);
+
+        // max and OptionalInt
+        OptionalInt maxCalories = Dish.menu.stream()
+                .mapToInt(Dish::getCalories)
+                .max();
+
+        int max = maxCalories.orElse(1);
+        System.out.println("Max calories: " + max);
+
+        // numeric ranges
+        IntStream evenNumbers = IntStream.rangeClosed(1, 100).filter(n -> n % 2 == 0);
+        System.out.println(evenNumbers.count());
+
+        Stream<int[]> pythagoreanTriples = IntStream
+                .rangeClosed(1, 100)
+                .boxed()
+                .flatMap(a -> IntStream.rangeClosed(a, 100)
+                        .filter(b -> Math.sqrt(a * a + b * b) % 1 == 0)
+                        .mapToObj(b -> new int[]{a, b, (int) Math.sqrt(a * a + b * b)}));
+        pythagoreanTriples.forEach(t -> System.out.println(t[0] + ", " + t[1] + ", " + t[2]));
+
+        Stream<int[]> pythagoreanTriples2 = IntStream.rangeClosed(1, 100).boxed()
+                .flatMap(a -> IntStream.rangeClosed(a, 100)
+                        .mapToObj(b -> new double[] {a, b, Math.sqrt(a * a + b * b)})
+                        .filter(t -> t[2] % 1 == 0))
+                .map(array -> Arrays.stream(array).mapToInt(a -> (int) a).toArray());
+        pythagoreanTriples2.forEach(t -> System.out.println(t[0] + ", " + t[1] + ", " + t[2]));
+    }
+}
