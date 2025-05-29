1ce47ec7c25c40c8032ac646cb14ada141e07e9b
yimengli <yimengcsu@gmail.com>
2025-05-29 19:31:22 +0200

    cnsia - ch02 - add HomeController

---
+++ cloud_native_spring_in_action/catalog-service/src/main/java/com/polarbookshop/catalog_service/HomeController.java
@@ -0,0 +1,14 @@
+package com.polarbookshop.catalog_service;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String getGreeting() {
+        return "Welcome to the book catalog!";
+    }
+}
